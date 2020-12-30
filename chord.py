f6d1698781b4a1cf8bddb3f46b3171215bfc8933
Eric Lemmon <ec.lemmon@gmail.com>
2020-12-30 11:57:52 +0100

    Building note and chord classes for neo-Riemannian and tonal relationship building

---
+++ chord.py
@@ -0,0 +1,19 @@
+from note import Note
+
+class Chord:
+    def __init__(self, *notes):
+        for i in notes:
+            assert isinstance(i, Note)
+        self.notes = notes
+
+    def __repr__(self):
+        result = ''
+        for note in self.notes:
+            result += " | " + str(note) + " | "
+        return result
+
+
+
+if __name__ == '__main__':
+    c_major = Chord(Note(60), Note(64), Note(67))
+    print(c_major)
---
+++ note.py
@@ -0,0 +1,22 @@
+class Note:
+    def __init__(self, midi_note_number):
+        self.midi_note_number = midi_note_number
+
+    def __repr__(self):
+        NOTES_FLAT = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
+        NOTES_SHARP = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+        octave = self.midi_note_number // 12 - 1
+        note_index = self.midi_note_number % 12
+        pretty_midi = "{flat}-{octave} or {sharp}-{octave}".format(
+            flat=NOTES_FLAT[note_index],
+            octave=octave,
+            sharp=NOTES_SHARP[note_index]
+        )
+        return pretty_midi
+
+if __name__ == '__main__':
+    c = Note(72)
+    db = Note(90)
+    print(c)
+    print(db)
+
