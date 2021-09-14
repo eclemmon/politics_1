b5be8cf52e7d2f129f9d0a4caf84c443d580a1d4
Eric Lemmon <ec.lemmon@gmail.com>
2021-09-14 14:38:36 +0200

    Profile reader script.

--- testing/profile_discourse_main.py
+++ testing/profile_discourse_main.py
@@ -1,1 +1,5 @@
 import cProfile
+import pstats
+
+p = pstats.Stats('/Users/ericlemmon/Google Drive/PhD/PhD_Project_v2/Profile_Files/output.txt')
+p.sort_stats(pstats.SortKey.CUMULATIVE).print_stats(100)
