584e9741cbc2fb6ca0353df9e7e1a9c5d6822146
Eric Lemmon <ec.lemmon@gmail.com>
2021-09-14 14:45:52 +0200

    Shell script for running profiler

---
+++ testing/profile_discourse_main.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /Users/ericlemmon/Google\ Drive/PhD/PhD_Project_v2/
+python -m cProfile -o /Users/ericlemmon/Google\ Drive/PhD/PhD_Project_v2/Profile_Files/output.txt  /Users/ericlemmon/Google\ Drive/PhD/PhD_Project_v2/1_Discourse/python_files/discourse_main.py
