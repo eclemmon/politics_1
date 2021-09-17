e42e264318faa4d092af3567fc9d4547c2c179e4
Eric Lemmon <ec.lemmon@gmail.com>
2021-09-17 07:35:06 +0200

    Music gen attack, support and sentiment analyser

--- 3_Techno_Autocracy/python_files/techno_autocracy_music_gen.py
+++ 3_Techno_Autocracy/python_files/techno_autocracy_music_gen.py
@@ -1,4 +1,5 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from NLP_Tools.sentiment_analysis_tools import get_average_sentiment
 
 
 class TechnoAutocracyMusicGen:
@@ -6,8 +7,19 @@ class TechnoAutocracyMusicGen:
         self.logger_object = logger_object
         self.sentiment_analyzer = sentiment_analyzer
 
-    def attack(self, message):
+    def attack(self):
+        pass
+
+    def support(self):
         pass
 
     def get_sentiment(self, message):
-        pass
+        return get_average_sentiment(self.sentiment_analyzer, message)
+
+    def trigger(self, message):
+        if self.get_sentiment(message)['compound'] < 0:
+            self.attack()
+        else:
+            self.support()
+
+
