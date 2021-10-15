de5c5a9f6ce6f5e08f5c2f4463bdda8ee626728a
Eric Lemmon <ec.lemmon@gmail.com>
2021-10-15 14:30:06 +0200

    unit tests for previous commit.

--- testing/unit_tests/test_map_vectors.py
+++ testing/unit_tests/test_map_vectors.py
@@ -1,4 +1,5 @@
 import unittest
+import math
 from Utility_Tools.map_vectors import *
 
 class TestMapVectors(unittest.TestCase):
@@ -28,3 +29,22 @@ class TestMapVectors(unittest.TestCase):
         self.assertAlmostEquals(vector_angle_from_pos_x_axis((-1, 1)), 2.3561944902)
         self.assertAlmostEquals(vector_angle_from_pos_x_axis((-1, -1)), -2.3561944902)
         self.assertAlmostEquals(vector_angle_from_pos_x_axis((0, -1)), -1.5707963268)
+
+    def test_vertice_angles_by_poly_size(self):
+        self.assertEquals(vertice_angles_by_poly_size(4), [0, 0.5 * math.pi, 1 * math.pi, 1.5 * math.pi - (2 * math.pi)])
+
+    def test_add_three_vectors_for_politics(self):
+        self.assertAlmostEquals(add_three_vectors_for_politics(1, 1, 1)[0], 0)
+        self.assertAlmostEquals(add_three_vectors_for_politics(1, 1, 1)[1], 0)
+
+    def test_get_closest_polygon_vertice_index_from_three_added_vectors(self):
+        pass
+
+    def test_find_index_of_closest_vector2vertice_in_new_poly(self):
+        self.assertEquals(find_index_of_closest_vector2vertice_in_new_poly(math.pi/2, 4), 1)
+        self.assertEquals(find_index_of_closest_vector2vertice_in_new_poly(math.pi/4, 4), 0)
+        self.assertEquals(find_index_of_closest_vector2vertice_in_new_poly(math.pi * 3 / 4, 4), 1)
+
+
+
+
