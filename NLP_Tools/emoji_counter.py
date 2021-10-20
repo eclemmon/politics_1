ae2c7e1b225a4151b4a374028bae75f5bc048dc8
Eric Lemmon <ec.lemmon@gmail.com>
2021-10-20 18:42:56 +0200

    Added tool to get sentiment values of individual Emojis

--- NLP_Tools/emoji_counter.py
+++ NLP_Tools/emoji_counter.py
@@ -1,10 +1,12 @@
 import emoji
+import emosent
 
 def is_emoji(token):
     if emoji.is_emoji(token):
         return True
     else:
         return False
+
 
 def count_emojis(tokens):
     count = 0
@@ -13,3 +15,10 @@ def count_emojis(tokens):
             count += 1
     return count
 
+
+def get_emoji_sentiment(token):
+    rank = emosent.get_emoji_sentiment_rank(token)
+    return {'neg': rank['negative'],
+            'neu': rank['neutral'],
+            'pos': rank['positive'],
+            'composite': rank['sentiment_score']}
