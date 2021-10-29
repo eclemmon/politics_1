d5a6edea75976a274dc60035b0ff70bb8bb5c216
Eric Lemmon <ec.lemmon@gmail.com>
2021-10-29 14:15:00 +0200

    IT'S ALIVE

--- scratch_scripts/flask_twilio_twitter_server.py
+++ scratch_scripts/flask_twilio_twitter_server.py
@@ -1,36 +1,34 @@
+import eventlet
+eventlet.monkey_patch()
 from flask import Flask, json, request
 from twilio.twiml.messaging_response import Message, MessagingResponse
 from flask_socketio import SocketIO
 import tweepy
 import json
 
+
 PATH = '/Users/ericlemmon/Google Drive/PhD/PhD_Project_v2/twitter_credentials.json'
 with open(PATH, "r") as file:
     credentials = json.load(file)
 
+
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'abc123'
-sio = SocketIO(app, cors_allowed_origins="*")
-
-auth = tweepy.OAuthHandler(credentials['CONSUMER_KEY'], credentials['CONSUMER_SECRET'])
-auth.set_access_token(credentials['ACCESS_TOKEN'], credentials['ACCESS_SECRET'])
-api = tweepy.API(auth)
+sio = SocketIO(app, message_queue='redis://', cors_allowed_origins="*")
 
 
-class MyListener(tweepy.StreamListener):
+class MyStream(tweepy.Stream):
+    def __init__(self, consumer_key, consumer_secret, access_token, access_secret):
+        super(MyStream, self).__init__(consumer_key, consumer_secret, access_token, access_secret)
+        self.stream_sio = SocketIO(message_queue='redis://')
+
     def on_status(self, status):
         print('status')
 
     def on_data(self, data):
-        handle_message(data)
-
-    def on_error(self, status):
-        print('error')
-        print(status)
-
-
-stream_listener = MyListener()
+        json_data = json.loads(data)
+        self.stream_sio.emit('handle_message', json_data['text'])
 
 
 @app.route('/sms', methods=['POST'])
@@ -48,9 +46,10 @@ def sms():
 def connect():
     print('connected')
     sio.emit('client_connected', "you connected")
-    search_term = "#ericlemmon"
-    stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
-    stream.filter(track=[search_term], is_async=True)
+    search_term = "#testingecl"
+    stream = MyStream(credentials['CONSUMER_KEY'], credentials['CONSUMER_SECRET'],
+                           credentials['ACCESS_TOKEN'], credentials['ACCESS_SECRET'])
+    stream.filter(track=[search_term], threaded=True)
     sio.emit('client_connected', "the search term is {}".format(search_term))
 
 
@@ -58,12 +57,8 @@ def connect():
 def disconnect():
     print('Client Diconnected')
 
-
-@sio.event
 def handle_message(message):
-    print("1", message)
     sio.emit('handle_message', message)
-
 
 if __name__ == '__main__':
     sio.run(app)
