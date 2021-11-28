6b6afd4d341a54a6e504e7d57e910f2cc1fb8249
Eric Lemmon <ec.lemmon@gmail.com>
2021-11-28 18:13:40 +0100

    created run.sh shell script.

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SC_DIR="~/Applications/SuperCollider.app/Contents/MacOS/"
+
+echo "##################"
+echo "Running Politics 1"
+echo "##################"
+
+command() {
+  sleep 5
+  python3.8 "$SCRIPT_DIR/Message_Endpoints/flask_twilio_twitter_client.py"
+}
+
+redis-server & python3.8 "$SCRIPT_DIR/Message_Endpoints/flask_twilio_twitter_server.py" & command
+exit
