c8ae1747ce92eabacae9809104472c7f634c1504
Eric Lemmon <ec.lemmon@gmail.com>
2022-01-24 17:56:21 -0500

    Created a data dump for pos tags in lists to draw upon

---
+++ Data_Dumps/penn_treebank_pos_tags.py
@@ -0,0 +1,3 @@
+# According to Oxford Dictionary pronouns are a subcategory of nouns
+penn_treebank_pos_nouns = ['CD', 'NN', 'NNS', 'NNP', 'NNPS', 'PRP', 'PRP$', 'WP', 'WP$']
+penn_treebank_pos_verbs = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
