b18c51eada232868d2ba4a45ffb2bf969602848d
Eric Lemmon <ec.lemmon@gmail.com>
2022-01-26 12:34:37 -0500

    generate_spatialization_values function.

--- Synthesis_Generators/spatialization_values_generator.py
+++ Synthesis_Generators/spatialization_values_generator.py
@@ -1,2 +1,15 @@
 # TODO: Data that determines spatialization <== Motion == starting point -> inverse starting point, time
 # TODO: {time = length of message. 280 max. Starting point = Composite Sentiment, Target = Inverse Sentiment val}
+
+def generate_spatialization_values(time, sentiment_dict):
+    starting_point = sentiment_dict['compound']
+    if starting_point > 0:
+        target_sent = 'neg'
+    elif starting_point == 0:
+        target_sent = 'neu'
+    else:
+        target_sent = 'pos'
+    target = sentiment_dict[target_sent]
+    return [time, starting_point, target]
+
+
