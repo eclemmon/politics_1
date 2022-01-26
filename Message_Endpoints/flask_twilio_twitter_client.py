cdbf4dc597b34ee06341cce0d89e0a91010bab73
Eric Lemmon <ec.lemmon@gmail.com>
2022-01-26 10:44:46 -0500

    Adding music_gen to flask_twilio_twitter_client.py

--- Message_Endpoints/flask_twilio_twitter_client.py
+++ Message_Endpoints/flask_twilio_twitter_client.py
@@ -1,7 +1,11 @@
 import socketio
+from a1_Discourse.python_files.discourse_music_gen import DiscourseMusicGen
+from Utility_Tools.politics_logger import logger_launcher
 
-
+# Initialize client, logger and music generator
 client = socketio.Client()
+logger = logger_launcher()
+music_gen = DiscourseMusicGen(logger)
 
 
 @client.on('client_connected')
@@ -13,6 +17,7 @@ def on_connect(message):
 def message(data):
     print("got data")
     print(data)
+    music_gen.trigger_sounds(data)
 
 
 client.connect('http://127.0.0.1:5000/')
