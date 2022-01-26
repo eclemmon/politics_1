4e9e8707e0c610b173648d47bd84e88217a59bcd
Eric Lemmon <ec.lemmon@gmail.com>
2022-01-26 12:36:50 -0500

    refactor name of variable starting_point to start_point

--- Synthesis_Generators/spatialization_values_generator.py
+++ Synthesis_Generators/spatialization_values_generator.py
@@ -1,15 +1,15 @@
-# TODO: Data that determines spatialization <== Motion == starting point -> inverse starting point, time
-# TODO: {time = length of message. 280 max. Starting point = Composite Sentiment, Target = Inverse Sentiment val}
+# Data that determines spatialization <== Motion == starting point -> inverse starting point, time
+# {time = length of message. 280 max. Starting point = Composite Sentiment, Target = Inverse Sentiment val}
 
 def generate_spatialization_values(time, sentiment_dict):
-    starting_point = sentiment_dict['compound']
-    if starting_point > 0:
+    start_point = sentiment_dict['compound']
+    if start_point > 0:
         target_sent = 'neg'
-    elif starting_point == 0:
+    elif start_point == 0:
         target_sent = 'neu'
     else:
         target_sent = 'pos'
     target = sentiment_dict[target_sent]
-    return [time, starting_point, target]
+    return [time, start_point, target]
 
 
