b7e239a56c87b117fedc48b3a790c028399f6954
Eric Lemmon <ec.lemmon@gmail.com>
2022-01-29 15:31:32 -0500

    Some reorganization

--- Testing/unit_tests/test_octave_displacement_functions.py
+++ Testing/unit_tests/test_octave_displacement_functions.py
@@ -1,12 +1,12 @@
 import unittest
-
-from Harmony_Generators.harmonic_walk_functions import get_octave_placement
+from Harmony_Generators.octave_displacement_generator import get_octave_placement
 
 max_chars = '*' * 280
 min_chars = ''
 medium_chars = '*' * 140
 
-class TestHarmonicWalkFunctions(unittest.TestCase):
+
+class TestOctaveDisplacementGenerator(unittest.TestCase):
     def test_get_octave_placement(self):
         self.assertEqual(get_octave_placement(max_chars), 8)
         self.assertEqual(get_octave_placement(min_chars), 1)
