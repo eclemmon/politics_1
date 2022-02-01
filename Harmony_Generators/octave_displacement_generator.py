f7412672634701f030a11a2553d615516f0fecd7
Eric Lemmon <ec.lemmon@gmail.com>
2022-01-31 21:53:40 -0500

    Updated octave_displacement_generator.py so that longer texts generate LOWER octaves and shorter HIGHER. Updated unit tests accordingly.

--- Harmony_Generators/octave_displacement_generator.py
+++ Harmony_Generators/octave_displacement_generator.py
@@ -3,4 +3,4 @@ from Utility_Tools.logistic_function import linear_to_logistic as l2l
 
 def get_octave_placement(text):
     length = len(text)
-    return int(l2l(length, 0, 280, 21, 108, 0.5)) // 12
+    return int(l2l(length, 0, 280, 132, 21, -0.5)) // 12
