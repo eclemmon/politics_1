c49a160f74624979eaf6c23b36a2ada7a946a0da
Eric Lemmon <ec.lemmon@gmail.com>
2022-02-02 07:32:15 -0500

    Updating client to automatically load instrument keys and names for music gen

--- Message_Endpoints/flask_twilio_twitter_client.py
+++ Message_Endpoints/flask_twilio_twitter_client.py
@@ -1,11 +1,14 @@
 import socketio
+from Data_Dumps.instrument_names import instrument_names
 from a1_Discourse.python_files.discourse_music_gen import DiscourseMusicGen
+from Synthesis_Generators.instrument_key_generator import InstrumentKeyAndNameGenerator
 from Utility_Tools.politics_logger import logger_launcher
 
 # Initialize client, logger and music generator
 client = socketio.Client()
 logger = logger_launcher()
-music_gen = DiscourseMusicGen(logger, )
+inst_key_name_gen = InstrumentKeyAndNameGenerator(instrument_names, 4)
+music_gen = DiscourseMusicGen(logger, inst_key_name_gen)
 
 
 @client.on('client_connected')
