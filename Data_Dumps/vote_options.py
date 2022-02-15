f587f1ebe8e7960af64b613af813e92dac997247
Eric Lemmon <ec.lemmon@gmail.com>
2022-02-15 14:55:52 -0500

    Added some silly options to work with in Cybernetic Republic.

---
+++ Data_Dumps/vote_options.py
@@ -0,0 +1,8 @@
+vote_options = {
+    'bass': ['funk', 'alberti', '808', 'sustained', 'walking', 'lament', 'random',
+             'polyrhythmic', 'on-beats'],
+    'melody': ['sustained', 'random', 'leapy', 'cats', 'choppy'],
+    'middle-voices': ['pads', 'random', 'choppy', 'off-beats'],
+    'harmony': ['doo-wop', 'weaponized', 'pachelbel', 'andalusian', 'changes', 'giant-steps', 'norm-core'],
+    'rhythm': ['four on the floor', 'polyrhythms', 'take-five', '']
+}
