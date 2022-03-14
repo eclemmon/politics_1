b6e870c22ab8de4f3ff77f9d40c1c1f502679293
Eric Lemmon <ec.lemmon@gmail.com>
2022-03-14 17:42:14 -0400

    Added twitter reply functionality

--- a1_Discourse/python_files/discourse_message_response.py
+++ a1_Discourse/python_files/discourse_message_response.py
@@ -1,14 +1,15 @@
-import os
+import tweepy
+import json
 from twilio.rest import Client
 from dotenv import dotenv_values
 
+def generate_discourse_message(data, names, values):
+    response_string = "Hi {}, thanks for the message, I processed it and it generated these values: \n"
 
-def generate_discourse_message(values):
-    return "lorem ipsum factum"
 
 
 class PoliticsMessageResponder:
-    def __init__(self, account_sid, auth_token, from_number):
+    def __init__(self, account_sid, auth_token, tweepy_auth, from_number):
         """
         Constructs the politics message responder that will be used to send messages back to users.
         :param account_sid: Twilio account session ID
@@ -16,6 +17,7 @@ class PoliticsMessageResponder:
         :param from_number: Phone number to send messages from (if, for example the phone number owned changes)
         """
         self.client = Client(account_sid, auth_token)
+        self.tweepy_api = tweepy.API(tweepy_auth)
         self.from_number = from_number
 
     def send_sms(self, message, to_number):
@@ -23,13 +25,36 @@ class PoliticsMessageResponder:
         Sends an sms message to the to_number
         :param message: String message to be sent
         :param to_number: String formatted "+19999999999"
-        :return: None
+        :return: True
         """
-        self.client.messages.create(
-            body=message,
-            from_=self.from_number,
-            to=to_number
-        )
+        try:
+            self.client.messages.create(
+                body=message,
+                from_=self.from_number,
+                to=to_number
+            )
+        except BaseException as e:
+            print("Error send_sms: {}".format(str(e)))
+        finally:
+            return True
+
+    def send_tweet(self, message, tweet_id):
+        """
+        Sents a tweet message as a reply to the tweet_id
+        :param message: String message to be tweeted.
+        :param tweet_id: Integer of tweet id to be responded to
+        :return: True
+        """
+        try:
+            self.tweepy_api.update_status(
+                status=message,
+                in_reply_to_status_id=tweet_id,
+                auto_populate_reply_metadata=True
+            )
+        except BaseException as e:
+            print("Error send_tweet: {}".format(str(e)))
+        finally:
+            return True
 
 
 if __name__ == "__main__":
