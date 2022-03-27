4d60676ef04ec52e7e800eaef9cf3ed05eddab34
Eric Lemmon <ec.lemmon@gmail.com>
2022-03-27 11:12:16 -0400

    Harmony class to store chord progressions & any necessary logic in the future.

---
+++ Classes/harmony.py
@@ -0,0 +1,20 @@
+from Classes.chord import Chord
+from Classes.note import Note
+
+
+class Harmony:
+    def __init__(self, chords):
+        for chord in chords:
+            assert isinstance(chord, Chord), "{} is expected to be a Chord class.".format(chord)
+        self.chords = chords
+
+    def __repr__(self):
+        return str([chord for chord in self.chords])
+
+
+if __name__ == "__main__":
+    c_major = Chord(Note(0), Note(4), Note(7))
+    CM7 = Chord(Note(0), Note(4), Note(7), Note(11))
+    chords = [c_major, CM7]
+    simple_harmony = Harmony(chords)
+    print(simple_harmony)
