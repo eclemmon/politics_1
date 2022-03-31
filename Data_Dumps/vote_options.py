8723079b555f5ebce20397bbefdd0b72562adf40
Eric Lemmon <ec.lemmon@gmail.com>
2022-03-31 08:24:10 -0400

    Correcting a rhythm key as amen-break is no longer an option. Instead a general, newly composed set of breakbeats have been composed

--- Data_Dumps/vote_options.py
+++ Data_Dumps/vote_options.py
@@ -1,8 +1,8 @@
 vote_options = {
-    'bass': ['funk', 'alberti', '808', 'sustained', 'walking', 'lament', 'random',
+    'bass': ['funk', 'alberti', 'sustained', 'walking', 'lament', 'random',
              'polyrhythmic', 'on-beats'],
     'melody': ['sustained', 'random', 'leapy', 'cats', 'choppy'],
     'middle-voices': ['pads', 'random', 'choppy', 'off-beats', 'arpeggiated'],
     'harmony': ['doo-wop', 'weaponized', 'pachelbel', 'andalusian', 'changes', 'giant-steps', 'norm-core'],
-    'rhythm': ['four-on-the-floor', 'polyrhythms', 'take-five', 'compound', 'amen-break']
+    'rhythm': ['four-on-the-floor', 'polyrhythms', 'take-five', 'compound', 'breakbeat']
 }
