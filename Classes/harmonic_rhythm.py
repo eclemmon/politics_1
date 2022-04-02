537c95bef6864a55de85f0bf8b3faeb3454af8c1
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-02 19:04:01 -0400

    Fixed a logic error where the chords were getting distributed over the meter of only a single bar, instead of the number of bars called by the class.

--- Classes/harmonic_rhythm.py
+++ Classes/harmonic_rhythm.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from Classes.meter import *
 from Classes.harmony import Harmony
 from Classes.chord import Chord
@@ -13,6 +15,8 @@ def split_off_duration(durations, index, amount=1):
 def recursive_split_meter(meter_subdivision, n_splits, n_recusions=0):
     if all(i == 1 for i in meter_subdivision):
         return [1 for _ in range(sum(meter_subdivision))]
+    elif n_recusions > n_splits:
+        return [sum(meter_subdivision)]
     elif n_recusions == n_splits:
         return meter_subdivision
     else:
@@ -25,17 +29,30 @@ def recursive_split_meter(meter_subdivision, n_splits, n_recusions=0):
 
 
 class HarmonicRhythm:
-    def __init__(self, meter, harmony, num_bars=4):
+    def __init__(self, meter, harmony, num_bars=4, hr_durations=None):
         self.meter = meter
         self.harmony = harmony
         self.num_bars = num_bars
-        self.hr_durations = self.get_hr_durations()
+        if hr_durations is None:
+            self.hr_durations = self.get_hr_durations()
+        else:
+            # 2d Array please!
+            self.hr_durations = hr_durations
+        self.flattened_hr_durations = [item for sublist in self.hr_durations for item in sublist]
 
     def get_hr_durations(self):
-        return recursive_split_meter(self.meter.subdivisions, len(self.harmony.chords) - len(self.meter.subdivisions))
+        # split harmony accross num_bars number of bars
+        chords = np.array_split(self.harmony.chords, self.num_bars)
+        # split the remaining harmony amongst by strong metrical subdivisions within the bar
+        res = []
+        for i in chords:
+            chords_subdivided_into_bar = i.tolist()
+            chords_durations = recursive_split_meter(self.meter.subdivisions, len(chords_subdivided_into_bar) - len(self.meter.subdivisions))
+            res.append(chords_durations)
+        return res
 
     def get_zipped_hr_chords_and_durations(self):
-        return [i for i in zip(self.harmony.chords, self.hr_durations)]
+        return [i for i in zip(self.harmony.chords, self.flattened_hr_durations)]
 
     def get_chords_and_durations(self):
         return [self.harmony.chords, self.flattened_hr_durations]
@@ -50,8 +67,9 @@ if __name__ == "__main__":
     c_major = Chord(Note(0), Note(4), Note(7))
     CM7 = Chord(Note(0), Note(4), Note(7), Note(11))
     Cmm7 = Chord(Note(0), Note(3), Note(7), Note(10))
+    a = Chord(Note(9), Note(0), Note(3))
     G7 = Chord(Note(7), Note(11), Note(14), Note(17))
-    harmony = Harmony([c_major, CM7, Cmm7, G7])
+    harmony = Harmony([c_major, CM7, Cmm7, a, G7])
     hrhythm = HarmonicRhythm(meter, harmony)
     print(hrhythm.get_zipped_hr_chords_and_durations())
     print(hrhythm.get_chords_and_durations())
@@ -60,6 +78,9 @@ if __name__ == "__main__":
     fhrhythm = HarmonicRhythm(five, harmony)
     print(fhrhythm.get_zipped_hr_chords_and_durations())
     print(fhrhythm.get_chords_and_durations())
+    print(fhrhythm.hr_durations)
     # print(hrhythm.build_phrase_accents_one())
     # print(hrhythm.base_case_one_chord())
     # print(hrhythm.base_case_two_chords())
+
+    print(fhrwd.get_zipped_hr_chords_and_durations())
