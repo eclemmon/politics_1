a0bc079d74512e110be57289080c68e10b0f9cfa
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-02 19:00:53 -0400

    Adjusted andalusian to chomatic-lament. Removed 'lament' bass as an option, as it would be defined by sustained + harmony: chromatic-lament or walking + harmony: chromatic-lament

--- Data_Dumps/vote_options.py
+++ Data_Dumps/vote_options.py
@@ -1,8 +1,7 @@
 vote_options = {
-    'bass': ['funk', 'alberti', 'sustained', 'walking', 'lament', 'random',
-             'polyrhythmic', 'on-beats'],
+    'bass': ['funk', 'alberti', 'sustained', 'walking', 'random', 'polyrhythmic', 'on-beats'],
     'melody': ['sustained', 'random', 'leapy', 'cats', 'choppy'],
     'middle-voices': ['pads', 'random', 'choppy', 'off-beats', 'arpeggiated'],
-    'harmony': ['doo-wop', 'weaponized', 'pachelbel', 'andalusian', 'changes', 'giant-steps', 'norm-core'],
+    'harmony': ['doo-wop', 'weaponized', 'pachelbel', 'chromatic-lament', 'changes', 'giant-steps', 'norm-core'],
     'rhythm': ['four-on-the-floor', 'polyrhythms', 'take-five', 'compound', 'breakbeat']
 }
