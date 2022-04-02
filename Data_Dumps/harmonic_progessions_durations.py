20409dc54dfda4fca62586703436c01a2705f127
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-02 19:01:16 -0400

    Data for harmonic progressions

---
+++ Data_Dumps/harmonic_progessions_durations.py
@@ -0,0 +1,81 @@
+from Classes.harmony import Harmony
+from Classes.chord import Chord
+from Classes.note import Note
+from Classes.harmonic_rhythm import HarmonicRhythm
+
+cybernetic_republic_harmonic_progressions = {
+    'doo-wop': [
+        Chord(Note(0), Note(4), Note(7), Note(0)),
+        Chord(Note(9), Note(0), Note(4), Note(9)),
+        Chord(Note(5), Note(0), Note(5), Note(9)),
+        Chord(Note(7), Note(11), Note(2), Note(7)),
+    ],
+    'weaponized': [
+        Chord(Note(9), Note(0), Note(4), Note(9)),
+        Chord(Note(5), Note(0), Note(5), Note(9)),
+        Chord(Note(0), Note(4), Note(7), Note(0)),
+        Chord(Note(7), Note(11), Note(2), Note(7))
+    ],
+    'pachelbel': [
+        Chord(Note(0), Note(7), Note(0), Note(3)),
+        Chord(Note(7), Note(7), Note(11), Note(2)),
+        Chord(Note(9), Note(4), Note(9), Note(0)),
+        Chord(Note(4), Note(4), Note(7), Note(11)),
+        Chord(Note(5), Note(0), Note(5), Note(9)),
+        Chord(Note(0), Note(0), Note(4), Note(7)),
+        Chord(Note(9), Note(0), Note(2), Note(5)),
+        Chord(Note(7), Note(11), Note(2), Note(5))
+    ],
+    'chromatic-lament': [
+        Chord(Note(0), Note(3), Note(7)),
+        Chord(Note(11), Note(7), Note(2)),
+        Chord(Note(10), Note(7), Note(0), Note(3)),
+        Chord(Note(9), Note(5), Note(0)),
+        Chord(Note(8), Note(3), Note(0), Note(6)),
+        Chord(Note(7), Note(3), Note(0)),
+        Chord(Note(7), Note(2), Note(11), Note(5))
+    ],
+    'changes': [
+        Chord(Note(4), Note(7), Note(11), Note(2)),  # Em7
+        Chord(Note(5), Note(9), Note(0), Note(3)),  # F7
+        Chord(Note(10), Note(2), Note(5), Note(9)),  # BbMM7
+        Chord(Note(1), Note(4), Note(8), Note(11)),  # Db7
+        Chord(Note(6), Note(10), Note(1), Note(5)),  # GbMM7
+        Chord(Note(9), Note(1), Note(4), Note(7)),  # A7
+        Chord(Note(2), Note(6), Note(9), Note(1)),  # DMM7
+        Chord(Note(2), Note(5), Note(9), Note(0)),  # Dmm7
+        Chord(Note(3), Note(7), Note(10), Note(1)),  # Eb7
+        Chord(Note(8), Note(0), Note(3), Note(7)),  # AbMM7
+        Chord(Note(11), Note(3), Note(6), Note(9)),  # B7
+        Chord(Note(4), Note(8), Note(11), Note(3)),  # EMM7
+        Chord(Note(7), Note(11), Note(2), Note(5)),  # G7
+        Chord(Note(0), Note(4), Note(7), Note(11)),  # CMM7
+    ],
+    'giant-steps': [
+        Chord(Note(11), Note(3), Note(6), Note(10)),  # BMM7
+        Chord(Note(2), Note(6), Note(9), Note(0)),  # D7
+        Chord(Note(7), Note(11), Note(2), Note(5)),  # GMM7
+        Chord(Note(10), Note(2), Note(5), Note(8)),  # Bb7
+        Chord(Note(3), Note(7), Note(10), Note(2)),  # EbMM7
+        Chord(Note(9), Note(0), Note(4), Note(7)),  # Amm7
+        Chord(Note(2), Note(6), Note(9), Note(0)),  # D7
+        Chord(Note(7), Note(11), Note(2), Note(5)),  # GMM7
+        Chord(Note(10), Note(2), Note(5), Note(8)),  # Bb7
+        Chord(Note(3), Note(7), Note(10), Note(2)),  # EbMM7
+        Chord(Note(6), Note(10), Note(1), Note(4)),  # F#7
+    ],
+    'norm-core': [
+        Chord(Note(0), Note(4), Note(7), Note(0)),
+        Chord(Note(7), Note(4), Note(7), Note(0)),
+        Chord(Note(0), Note(4), Note(7), Note(0)),
+        Chord(Note(7), Note(4), Note(7), Note(0)),
+        Chord(Note(0), Note(4), Note(7), Note(0)),
+        Chord(Note(7), Note(4), Note(7), Note(0)),
+        Chord(Note(0), Note(4), Note(7), Note(0)),
+        Chord(Note(7), Note(4), Note(7), Note(0)),
+    ]
+}
+
+
+def build_harmonic_rhythm(meter, chords):
+    return HarmonicRhythm(meter, Harmony(chords))
