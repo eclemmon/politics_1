54fdaccd0618f3dd818921da5d311545f356a7a8
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-03 19:37:06 -0400

    Added random as a option in the rhythms

--- Data_Dumps/vote_options.py
+++ Data_Dumps/vote_options.py
@@ -3,5 +3,5 @@ vote_options = {
     'melody': ['sustained', 'random', 'leapy', 'cats', 'choppy'],
     'middle-voices': ['pads', 'random', 'choppy', 'off-beats', 'arpeggiated'],
     'progression': ['doo-wop', 'weaponized', 'pachelbel', 'chromatic-lament', 'changes', 'giant-steps', 'norm-core'],
-    'rhythm': ['four-on-the-floor', 'polyrhythms', 'take-five', 'compound', 'breakbeat']
+    'rhythm': ['four-on-the-floor', 'polyrhythms', 'take-five', 'compound', 'breakbeat', 'random']
 }
