b0c45e239730597da99a41ea3ca6b0eebf85abd4
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-04 22:41:41 -0400

    Melody init commit

---
+++ Classes/melody.py
@@ -0,0 +1,8 @@
+import Classes.harmonic_rhythm
+import Classes.scale
+
+
+class Melody:
+    def __init__(self, harmonic_rhythm, scale):
+        self.harmonic_rhythm = harmonic_rhythm
+        self.scale = scale
