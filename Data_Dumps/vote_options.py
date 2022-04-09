f2034bf3d24e92590772296db0117ad5df79a7dd
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-09 15:46:34 -0400

    Removed cats from melody.

--- Data_Dumps/vote_options.py
+++ Data_Dumps/vote_options.py
@@ -1,6 +1,6 @@
 vote_options = {
     'bass': ['funk', 'alberti', 'sustained', 'walking', 'random', 'polyrhythmic', 'on-beats'],
-    'melody': ['sustained', 'random', 'leapy', 'cats', 'choppy'],
+    'melody': ['sustained', 'random', 'leapy', 'choppy', 'polyrhythmic'],
     'middle-voices': ['pads', 'random', 'choppy', 'off-beats', 'arpeggiated'],
     'progression': ['doo-wop', 'weaponized', 'pachelbel', 'chromatic-lament', 'changes', 'giant-steps', 'norm-core'],
     'rhythm': ['four-on-the-floor', 'polyrhythms', 'take-five', 'compound', 'breakbeat', 'random']
