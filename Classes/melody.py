c7f03521e237253b0116abb86da22bf1d43157da
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-09 15:46:46 -0400

    Imports in melody.py

--- Classes/melody.py
+++ Classes/melody.py
@@ -1,8 +1,29 @@
 import Classes.harmonic_rhythm
 import Classes.scale
+import random
+
+from Classes.note import Note
+from Classes.chord import Chord
 
 
 class Melody:
     def __init__(self, harmonic_rhythm, scale):
         self.harmonic_rhythm = harmonic_rhythm
         self.scale = scale
+
+    @staticmethod
+    def build_rest(duration=1):
+        return '/r' + str(duration)
+
+    @staticmethod
+    def is_rest(probability=0.50):
+        return random.random() < probability
+
+    def make_note_or_rest(self, duration=1, probability=0.50):
+        if self.is_rest(probability):
+            return self.build_rest(duration)
+        else:
+            return duration
+
+    def build_notes_and_durations(self):
+        pass
