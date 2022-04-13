0851a97e603ebeefd39c7a5bdfd48413894a9c54
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-13 13:45:37 -0400

    rhythm_section_data.py init commit

---
+++ Data_Dumps/rhythm_section_data.py
@@ -0,0 +1,10 @@
+from Classes.rhythm_section import *
+
+cybernetic_republic_rhythm_section = {
+    'four-on-the-floor': FourOnTheFloor,
+    'polyrhythms': PolyRhythms,
+    'take-five': TakeFive,
+    'compound': CompoundSix,
+    'breakbeat': BreakBeat,
+    'random': RandomRhythms
+}
