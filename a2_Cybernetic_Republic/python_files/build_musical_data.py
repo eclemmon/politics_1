eddba9f7df25b01165364c3618360202b091abcc
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-13 15:25:48 -0400

    Corrected imports

--- a2_Cybernetic_Republic/python_files/build_musical_data.py
+++ a2_Cybernetic_Republic/python_files/build_musical_data.py
@@ -5,7 +5,6 @@ from Data_Dumps.bass_data import cybernetic_republic_basses
 from Data_Dumps.middle_voices_data import cybernetic_republic_middle_voices
 from Data_Dumps.rhythm_section_data import cybernetic_republic_rhythm_section
 from Classes.harmonic_rhythm import HarmonicRhythm
-from Classes.melody import *
 
 
 def build_cybernetic_harmonic_rhythm(meter_key, progression_key):
