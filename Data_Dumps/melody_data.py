fc43e03bad7ed123d01e463543fc02f86671b3d3
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-13 09:02:55 -0400

    Add melody_data.py

---
+++ Data_Dumps/melody_data.py
@@ -0,0 +1,10 @@
+from Classes.melody import *
+
+cybernetic_republic_melodies = {
+    'sustained': SustainedMelody,
+    'random': RandomMelody,
+    'leapy': LeapyMelody,
+    'choppy': ChoppyMelody,
+    'polyrhythmic': PolyrhythmicMelody,
+    'maximum-ornamentation': MaxOrnamentationMelody
+}
