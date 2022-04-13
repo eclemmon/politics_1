773ea742d34ba6505e392586f8d4659e5f5c396c
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-13 16:10:42 -0400

    Added meter to vote options.

--- Data_Dumps/vote_options.py
+++ Data_Dumps/vote_options.py
@@ -3,7 +3,9 @@ vote_options = {
     'melody': ['sustained', 'random', 'leapy', 'choppy', 'polyrhythmic', 'maximum-ornamentation'],
     'middle-voices': ['pads', 'random', 'choppy', 'off-beats', 'arpeggiated'],
     'progression': ['doo-wop', 'weaponized', 'pachelbel', 'chromatic-lament', 'changes', 'giant-steps', 'norm-core'],
-    'rhythm': ['four-on-the-floor', 'polyrhythms', 'take-five', 'compound', 'breakbeat', 'random']
+    'rhythm': ['four-on-the-floor', 'polyrhythms', 'take-five', 'compound', 'breakbeat', 'random'],
+    'meter': ['two', 'three', 'four', 'five-2+3', 'five-2+3', 'six', 'seven-2+2+3', 'seven-2+3+2', 'seven3+2+2', 'nine',
+              'twelve']
 }
 
 # DONE: Rhythm, Progression, Middle Voices
