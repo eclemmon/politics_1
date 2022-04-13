f08dbe9ec6af9a97929665d56302a8f5ea5c60e0
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-13 13:51:04 -0400

    build_musical_data function

--- a2_Cybernetic_Republic/python_files/build_musical_data.py
+++ a2_Cybernetic_Republic/python_files/build_musical_data.py
@@ -3,6 +3,7 @@ from Data_Dumps.meter_data import cybernetic_republic_meter_data
 from Data_Dumps.melody_data import cybernetic_republic_melodies
 from Data_Dumps.bass_data import cybernetic_republic_basses
 from Data_Dumps.middle_voices_data import cybernetic_republic_middle_voices
+from Data_Dumps.rhythm_section_data import cybernetic_republic_rhythm_section
 from Classes.harmonic_rhythm import HarmonicRhythm
 from Classes.melody import *
 
@@ -21,3 +22,7 @@ def build_bass(bass_key, harmonic_rhythm, scale):
 
 def build_middle_voices(middle_voices_key, harmonic_rhythm):
     return cybernetic_republic_middle_voices[middle_voices_key](harmonic_rhythm)
+
+
+def build_rhythm_section(rhythm_section_key, meter):
+    return cybernetic_republic_rhythm_section[rhythm_section_key](meter)
