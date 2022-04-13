57fb08173baf80eb161ae7f2560ac2bf07f0c6f1
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-13 13:45:53 -0400

    middle_voices_data.py init commit

---
+++ Data_Dumps/middle_voices_data.py
@@ -0,0 +1,9 @@
+from Classes.middle_voices import *
+
+cybernetic_republic_middle_voices = {
+    'pads': Pads,
+    'random': RandomMiddleVoices,
+    'choppy': ChoppyMiddleVoices,
+    'off-beats': OffBeatMiddleVoices,
+    'arpeggiated': ArpeggiatedMiddleVoices
+}
