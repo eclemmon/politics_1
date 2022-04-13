87792d0ff317caf9c6653e8ec23df6ffa56c45c9
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-12 21:32:54 -0400

    bass or melody OSCfunc outputs

--- a2_Cybernetic_Republic/python_files/send_to_sc_functions.py
+++ a2_Cybernetic_Republic/python_files/send_to_sc_functions.py
@@ -26,6 +26,29 @@ def send_middle_voice_initialization_to_sc(channel, sc_client, address='/middle_
     sc_client.send(msg)
 
 
+def send_bass_or_melody_notes_to_sc(b_or_m, sc_client, address):
+    for note in b_or_m.chords_and_durations[0]:
+        msg = osc_message_builder.OscMessageBuilder(address=address)
+        msg.add_arg(note.midi_note_number, 'i')
+        msg = msg.build()
+        sc_client.send(msg)
+
+
+def send_bass_or_melody_durations_to_sc(b_or_m, sc_client, address):
+    msg = osc_message_builder.OscMessageBuilder(address=address)
+    for duration in b_or_m.chords_and_durations[1]:
+        msg.add_arg(duration)
+    msg = msg.build()
+    sc_client.send(msg)
+
+
+def send_bass_or_melody_initialization_to_sc(channel, sc_client, address):
+    msg = osc_message_builder.OscMessageBuilder(address=address)
+    msg.add_arg(channel, arg_type='i')
+    msg = msg.build()
+    sc_client.send(msg)
+
+
 def send_arpeggiator_on_off_to_sc(sc_client, address='/arpeggiator'):
     msg = osc_message_builder.OscMessageBuilder(address=address)
     msg = msg.build()
