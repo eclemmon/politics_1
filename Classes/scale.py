4bdc578c2149300f7f8f30067f983b5a2c32c632
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-13 21:11:38 -0400

    Updates to scale to allow for the returning of new scale instances depending on the function called.

--- Classes/scale.py
+++ Classes/scale.py
@@ -4,7 +4,7 @@ from Classes.note import Note
 class Scale:
     def __init__(self, *notes):
         for i in notes:
-            assert isinstance(i, Note)
+            assert isinstance(i, Note) or isinstance(i, tuple)
         self.notes = notes
 
     def __eq__(self, other):
@@ -21,10 +21,10 @@ class Scale:
             note.transpose(num)
 
     def modal_transpose(self, scale_degree):
-        self.notes = tuple(self.notes[scale_degree:] + self.note[:scale_degree])
+        self.notes = tuple(self.notes[scale_degree:] + self.notes[:scale_degree])
 
     def modal_transpose_return_new(self, scale_degree):
-        return Scale(self.notes[scale_degree:] + self.note[:scale_degree])
+        return Scale(tuple(self.notes[scale_degree:] + self.notes[:scale_degree]))
 
     def transpose_return_new(self, num):
-        return Scale([Note(note.midi_note_number + num) for note in self.notes])
+        return Scale(tuple([Note(note.midi_note_number + num) for note in self.notes]))
--- scratch_scripts/send_to_sc_testing.py
+++ scratch_scripts/send_to_sc_testing.py
@@ -1,11 +1,26 @@
 from a2_Cybernetic_Republic.python_files.build_musical_data import *
 from a2_Cybernetic_Republic.python_files.send_to_sc_functions import *
-from Data_Dumps.bass_data import cybernetic_republic_basses
-from Data_Dumps.melody_data import cybernetic_republic_melodies
-from Data_Dumps.meter_data import cybernetic_republic_meter_data
-from Data_Dumps.progession_data import cybernetic_republic_progressions
-from Data_Dumps.rhythm_section_data import cybernetic_republic_rhythm_section
+from Data_Dumps.scale_data import cybernetic_republic_scales
+from pythonosc import udp_client
 
-hr = build_harmonic_rhythm('four', 'chromatic-lament')
-bass = build_bass('walking')
-melody = build_melody('choppy', hr)
+sc_client = udp_client.SimpleUDPClient("127.0.0.1", 57120)
+
+hr = build_harmonic_rhythm('five-2+3', 'giant-steps')
+bass = build_bass('sustained', hr, cybernetic_republic_scales['major'])
+melody = build_melody('leapy', hr, cybernetic_republic_scales['major'])
+rhythm = build_rhythm_section('take-five', 'five-2+3')
+middle_voices = build_middle_voices('pads', hr)
+
+send_rhythm_to_sc(rhythm, sc_client)
+send_rhythm_initialization_to_sc(sc_client)
+send_bass_or_melody_notes_to_sc(bass, sc_client, '/bass_notes')
+send_bass_or_melody_durations_to_sc(bass, sc_client, '/bass_durations')
+send_bass_or_melody_initialization_to_sc(0, sc_client, '/bass_init')
+
+send_bass_or_melody_notes_to_sc(melody, sc_client, '/melody_notes')
+send_bass_or_melody_durations_to_sc(melody, sc_client, '/melody_durations')
+send_bass_or_melody_initialization_to_sc(0, sc_client, '/melody_init')
+
+send_middle_voice_chords_to_sc(middle_voices, sc_client)
+send_middle_voice_durations_to_sc(middle_voices, sc_client)
+send_middle_voice_initialization_to_sc(0, sc_client)
