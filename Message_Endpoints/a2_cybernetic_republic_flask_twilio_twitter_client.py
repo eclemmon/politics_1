70c6f7f6e8fc884423aa99c17fcb31336bf344b2
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-17 10:56:32 -0400

    a2_cybernetic_republic_flask_twilio_twitter_client.py init commit

---
+++ Message_Endpoints/a2_cybernetic_republic_flask_twilio_twitter_client.py
@@ -0,0 +1,36 @@
+import socketio
+import json
+from a2_Cybernetic_Republic.python_files.cybernetic_republic_music_gen import CyberneticRepublicMusicGen
+from Data_Dumps.vote_options import vote_options
+from Utility_Tools.politics_logger import logger_launcher
+
+# Load settings
+SETTINGS_PATH = '../settings.json'
+with open(SETTINGS_PATH, "r") as file:
+    settings = json.load(file)
+
+TWITTER_PATH = '../twitter_credentials.json'
+with open(TWITTER_PATH, "r") as file:
+    credentials = json.load(file)
+
+# Initialize client, logger and music generator
+client = socketio.Client()
+logger = logger_launcher()
+
+music_gen = CyberneticRepublicMusicGen(logger, vote_options, num_cycles=20)
+
+
+@client.on('client_connected')
+def on_connect(message):
+    print(message)
+
+
+@client.on('handle_message')
+def message(data):
+    print(data)
+    music_gen.on_data(data)
+
+
+music_gen.run()
+client.connect('http://127.0.0.1:8000/')
+client.wait()
