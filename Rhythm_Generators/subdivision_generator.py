4375f33b06f6cf22a714da4524d1db2cfaba6f1d
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-20 17:36:25 -0400

    Documentation for subdivision_generator.py

--- Rhythm_Generators/subdivision_generator.py
+++ Rhythm_Generators/subdivision_generator.py
@@ -1,3 +1,9 @@
 
-def subdivide_meter_into_polyrhythm(num_beats, subdivided_by):
+def subdivide_meter_into_polyrhythm(num_beats: int, subdivided_by: int):
+    """
+    Helper function for subdividing the total duration of a meter into n number of subdivisions.
+    :param num_beats: int
+    :param subdivided_by: int
+    :return: list of floats
+    """
     return [num_beats * 0.25 * 4 / subdivided_by for _ in range(subdivided_by)]
