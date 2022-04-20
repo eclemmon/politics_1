b0f49cb6b4205ae924010ae55dd44326bb8d0f90
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-19 21:55:41 -0400

    Abosulte paths for Settings and Twitter keys

--- Message_Endpoints/a2_cybernetic_republic_flask_twilio_twitter_client.py
+++ Message_Endpoints/a2_cybernetic_republic_flask_twilio_twitter_client.py
@@ -4,12 +4,14 @@ from a2_Cybernetic_Republic.python_files.cybernetic_republic_music_gen import Cy
 from Data_Dumps.vote_options import vote_options
 from Utility_Tools.politics_logger import logger_launcher
 
+
+
 # Load settings
-SETTINGS_PATH = '../settings.json'
+SETTINGS_PATH = '/Users/ericlemmon/Documents/PhD/PhD_Project_v2/settings.json'
 with open(SETTINGS_PATH, "r") as file:
     settings = json.load(file)
 
-TWITTER_PATH = '../twitter_credentials.json'
+TWITTER_PATH = '/Users/ericlemmon/Documents/PhD/PhD_Project_v2/twitter_credentials.json'
 with open(TWITTER_PATH, "r") as file:
     credentials = json.load(file)
 
@@ -17,7 +19,7 @@ with open(TWITTER_PATH, "r") as file:
 client = socketio.Client()
 logger = logger_launcher()
 
-music_gen = CyberneticRepublicMusicGen(logger, vote_options, num_cycles=20)
+music_gen = CyberneticRepublicMusicGen(logger, vote_options, num_cycles=21)
 
 
 @client.on('client_connected')
