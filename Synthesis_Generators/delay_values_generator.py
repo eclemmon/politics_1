bf5cb2c5232798f325b8e85753c8df63a09cb055
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-20 17:37:36 -0400

    documentation for delay_values_generator.py

--- Synthesis_Generators/delay_values_generator.py
+++ Synthesis_Generators/delay_values_generator.py
@@ -1,15 +1,15 @@
 from Data_Dumps import pos_tags
 
 
-def delay_time_and_decay(pos_count_dict,
-                         noun_tags=pos_tags.penn_treebank_pos_nouns,
-                         verb_tags=pos_tags.penn_treebank_pos_verbs):
+def delay_time_and_decay(pos_count_dict: dict,
+                         noun_tags: list = pos_tags.penn_treebank_pos_nouns,
+                         verb_tags: list = pos_tags.penn_treebank_pos_verbs):
     """
     This function counts the number of nouns and verbs in a string and returns them as a tuple.
-    :param pos_count_dict: Dictionary that maps pos tag as string to an integer count e.g. {'NN': 4, 'VB': 2}
-    :param noun_tags: A list of noun pos tags based on the tagger used. e.g. ['NN', 'NNS', etc...]
-    :param verb_tags: A list of verb pos tags based on the tagger used. e.g. ['VB', 'VBZ', etc...]
-    :return: Tuple e.g. (4, 2)
+    :param pos_count_dict: dict that maps pos tag as string to an integer count e.g. {'NN': 4, 'VB': 2}
+    :param noun_tags: list of noun pos tags based on the tagger used. e.g. ['NN', 'NNS', etc...]
+    :param verb_tags: list of verb pos tags based on the tagger used. e.g. ['VB', 'VBZ', etc...]
+    :return: tuple e.g. (4, 2)
     """
     nouns = 0
     verbs = 0
