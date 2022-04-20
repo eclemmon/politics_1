ff4004a3b7164020229c30a81f82c2801816a19e
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-19 21:54:21 -0400

    .sh file to boot cybernetic republic movement

---
+++ a2_Cybernetic_Republic/run_cybernetic_republic.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+PYTHONPATH=/Users/ericlemmon/Documents/PhD/PhD_Project_v2/ /Users/ericlemmon/Documents/PhD/PhD_Project_v2/venv/bin/python /Users/ericlemmon/Documents/PhD/PhD_Project_v2/Message_Endpoints/a2_cybernetic_republic_flask_twilio_twitter_client.py
