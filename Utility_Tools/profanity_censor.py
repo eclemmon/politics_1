1ccd10897f74877fa759d741453f3042403a8ad6
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-20 18:17:20 -0400

    documentation for cenor

--- Utility_Tools/profanity_censor.py
+++ Utility_Tools/profanity_censor.py
@@ -2,22 +2,35 @@ from better_profanity import profanity
 
 
 class Censor:
-    def __init__(self, censor_level=0):
+    """
+    Censor class to censor a string so that trolls can't do their thing.
+    """
+    def __init__(self, censor_level: int = 0):
+        """
+        Initialization for Censor
+        :param censor_level: int
+        """
         self.censor_level = censor_level
         if self.censor_level >= 1:
             profanity.load_censor_words()
 
     @staticmethod
     def censor_on(string):
+        """
+        Censors the given string.
+        :param string: str
+        :return: str
+        """
         return profanity.censor(string)
 
-    @staticmethod
-    def censor_off(string):
-        return string
-
     def censor(self, string):
+        """
+        General function that either censors a string or doesn't based on this input censor_level
+        :param string: str
+        :return: str
+        """
         if self.censor_level == 0:
-            return self.censor_off(string)
+            return string
         else:
             return self.censor_on(string)
 
