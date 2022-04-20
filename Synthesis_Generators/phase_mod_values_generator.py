d54f7c85218341f4d00e40c38b09837eae895c8e
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-20 17:46:22 -0400

    Documentation for phase_mod_values_generator.py

--- Synthesis_Generators/phase_mod_values_generator.py
+++ Synthesis_Generators/phase_mod_values_generator.py
@@ -1,8 +1,13 @@
-from NLP_Tools.emoji_counter import get_average_emoji_sent_from_msg
-from NLP_Tools.emoji_counter import get_emojis
+from NLP_Tools.sentiment_dictionary import SentimentDict
 
 
-def phase_mod_values_generator(avg_emoji_sent, emojis):
+def phase_mod_values_generator(avg_emoji_sent: SentimentDict, emojis: str):
+    """
+    Function to generate values for phase modulation in SuperCollider by reading sentiment of emojis and num of emojis.
+    :param avg_emoji_sent: SentimentDict
+    :param emojis: str
+    :return: tuple of (float, float)
+    """
     if avg_emoji_sent is None:
         return 0, 0
     else:
