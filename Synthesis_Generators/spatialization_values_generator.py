584720ad3497e29a7c0f123b4027d66930a9c1fa
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-20 17:50:06 -0400

    documentation for spatialization_values_generator.py

--- Synthesis_Generators/spatialization_values_generator.py
+++ Synthesis_Generators/spatialization_values_generator.py
@@ -1,7 +1,17 @@
-# Data that determines spatialization <== Motion == starting point -> inverse starting point, time_interval
-# {time_interval = length of message. 280 max. Starting point = Composite Sentiment, Target = Inverse Sentiment val}
 
-def generate_spatialization_values(time_interval, sentiment_dict):
+def generate_spatialization_values(time_interval: int, sentiment_dict: dict):
+    """
+    Generates spatialization values. Data that determines spatialization:
+    Motion = starting point to inverse starting point,
+    time_interval = length of message. 280 max.
+    Starting point = Composite Sentiment,
+    Target = Inverse Sentiment val
+    So if composite sentiment is -0.5 in value, will find the value of the avg. positive sentiment of the document
+    and use that as the target destination.
+    :param time_interval: int
+    :param sentiment_dict: dict
+    :return: list of floats
+    """
     start_point = sentiment_dict['compound']
     if start_point > 0:
         target_sent = 'neg'
