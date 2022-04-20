44600b3db88984b8ee799625d422588363a9b19c
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-20 16:03:04 -0400

    documentation for neighbor_chord_generator.py

--- Harmony_Generators/neighbor_chord_generator.py
+++ Harmony_Generators/neighbor_chord_generator.py
@@ -1,12 +1,25 @@
 from Utility_Tools.map_vectors import get_graph_chord_indexes_and_weights
 
 
-def generate_neighbor_chord_weights(sentiment_dict, num_adj_chords):
+def generate_neighbor_chord_weights(sentiment_dict: dict, num_adj_chords: int):
+    """
+    Function to generate weights for which notes from neighbor chords are stochastically called.
+    :param sentiment_dict: dict
+    :param num_adj_chords: int
+    :return: dict
+    """
     sent = {'neg': sentiment_dict['neg'], 'neu': sentiment_dict['neu'], 'pos': sentiment_dict['pos']}
     return get_graph_chord_indexes_and_weights(sent, num_adj_chords)
 
 
 def build_weight_and_chord_array(current_chord, neighbor_notes, weights):
+    """
+    Helper function for associating chords with generated weights in a parseable manner for SuperCollider
+    :param current_chord: list of Notes
+    :param neighbor_notes: list of Notes
+    :param weights: list of Floats
+    :return: list
+    """
     res = []
     for k, v in weights.items():
         if k == 'home':
