3ba38d8e9ec08294efb1a3324f4fdc7e555f9be8
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-20 17:51:29 -0400

    Documentation for database_reset.py

--- Utility_Tools/database_reset.py
+++ Utility_Tools/database_reset.py
@@ -3,6 +3,12 @@ from flask_twilio_twitter_server import app
 
 
 def reset_db(app, db):
+    """
+    Resets the app's database. WARNING! WILL DELETE ALL THE TEXTS YOU GOT! :,(
+    :param app: Flask app
+    :param db: Database
+    :return: None
+    """
     with app.app_context():
         db.drop_all()
         db.create_all()
