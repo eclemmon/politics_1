06e03c346f7124854e04e20f48e696b12787edfe
Eric Lemmon <ec.lemmon@gmail.com>
2022-04-27 10:38:26 -0400

    Some start documents for text relationship mapping

---
+++ Data_Dumps/primary_political_documents.py
@@ -0,0 +1,23 @@
+"""
+primary_political_documents.py
+"""
+
+__author__ = "Eric Lemmon"
+__copyright__ = "Copyright 2022, Eric Lemmon"
+__credits__ = ["Eric Lemmon"]
+__version__ = ""
+__maintainer__ = "Eric Lemmon"
+__email__ = "ec.lemmon@gmail.com"
+__status__ = "Production"
+
+init_political_documents = [
+    "But the chief penalty is to be governed by someone worse if one will not themselves hold office and rule.",
+    "Now indeed, you, young man, are the king. The foundation of your government will be based upon those whom you "
+    "choose to appoint to the key posts.",
+    'All states and all dominions that have had or now have authority over men have been and now are either republics '
+    'or princedoms.',
+    "Be just then, O ye men of understanding! and mark not more severely what women do amiss, than the vicious tricks "
+    "of the horse or the ass for whom ye provide provender, and allow her the privileges of ignorance, to whom ye "
+    "deny the rights of reason, or ye will be worse than Egyptian task-masters, expecting virtue where nature has not "
+    "given understanding!"
+]
