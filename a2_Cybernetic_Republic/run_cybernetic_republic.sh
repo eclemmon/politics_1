c9fdb4debd2fa93c8bf815df00aeac0a1ffc76ea
Eric Lemmon <ec.lemmon@gmail.com>
2022-08-08 17:17:47 -0400

    Extend sleep time as a crutch for running cybernetic republic.

--- a2_Cybernetic_Republic/run_cybernetic_republic.sh
+++ a2_Cybernetic_Republic/run_cybernetic_republic.sh
@@ -20,7 +20,6 @@ echo "Virtual environment successfully booted"
 
 processing-java --sketch="$ROOT_DIR/a2_Cybernetic_Republic/visualization_files/a2_cybernetic_republic/" --run &
 "$SC_DIR/sclang" "$SC_SCRIPT_PATH" &
-sleep 10
+sleep 45
 "$ROOT_DIR/venv/bin/python" "$ROOT_DIR/Message_Endpoints/a2_cybernetic_republic_flask_twilio_twitter_client.py"
-
 
